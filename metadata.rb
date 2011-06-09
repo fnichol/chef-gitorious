ffa37fdefe39cea996e9e58108d3df76a7654554
Fletcher Nichol <fnichol@nichol.ca>
2011-06-08 21:29:42 -0600

    Fix readme link in metadata.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Fletcher Nichol"
 maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Installs/Configures gitorious"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
 depends          "rvm"
 depends          "rvm_passenger"
